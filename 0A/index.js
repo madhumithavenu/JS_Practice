5ad484103e54d8d02c33a2bb6c2b9a1023c08e6e
Madhumitha <madhumithavenu03@gmail.com>
2023-07-25 11:14:03 +0530

    Unary Arthematic Operators

--- 0A/index.js
+++ 0A/index.js
@@ -47,9 +47,44 @@ console.log('VAnaKam'.toLowerCase());
 console.log('Super Ranga'.startsWith('S'));
 console.log('Super Ranga'.startsWith('R'));
 
+console.log('               No Space Please               '.trim());
+
 //Build-in Objects
 console.log(Math.random());
 console.log(Math.floor(46.7)); //46
 console.log(Math.ceil(46.7)); //47
 console.log(Number.isInteger(75)); //true
 console.log(Number.isInteger(75.33)); //false
+
+//Create and use Variables
+var myName ='Madhu';
+var age =21;
+
+console.log(myName);
+console.log(age);
+console.log('My name is ' + myName + ' & my age is ' + age);
+
+//Local Variables using :let
+let myName ='Nischal';
+let age =15;
+
+console.log(myName);
+console.log(age);
+console.log('My name is ' + myName + ' & my age is ' + age);
+
+//Constant - are very similar to variables whose values cannot be re-assigned anywhere in the program.
+//Constants are declared using the keyword 'const' in javascript.
+//If we try to change the value of the constant we get an error.
+
+const myMom = 'Divya';
+const age = 34;
+console.log('My mom is ' +myMom+ ' & her age is ' +age);
+
+//Unary Arthematic Operator
+var num =55;
+
+num += 10; //65 | Equivalent: num = num + 10;
+num -= 20; //45 | Equivalent: num = num - 20;
+num *= 100; //4500 | Equivalent: num = num * 100;
+num /= 50; //90 | Equivalent: num = num / 50;
+num %= 4; //2 | Equivalent: num = num % 4;
