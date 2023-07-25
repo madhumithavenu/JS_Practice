5ddddb1cd61476513c9ae57dc38ad69b909ce106
Madhumitha <madhumithavenu03@gmail.com>
2023-07-25 10:44:08 +0530

    String Methods- trim

--- 0A/index.js
+++ 0A/index.js
@@ -8,20 +8,48 @@ console.log(2002);*/
 // There are six primary data-types in JavaScript(excluding symbol)
 // a)Number b)String c)Boolean d)Null e)Undefined f)Object
 
-console.log(5);
-console.log(25.69);
+// console.log(5);
+// console.log(25.69);
 
-console.log('JavaScript');
-console.log('My name is Madhu! I love #JScript');
+// console.log('JavaScript');
+// console.log('My name is Madhu! I love #JScript');
 
-console.log(5 > 6);
-console.log(5<=25);
+// console.log(5 > 6);
+// console.log(5<=25);
 
 // Null -vs- Undefined
 
-var a = null;
-console.log(a);
+// var a = null;
+// console.log(a);
 
+// var b;
+// console.log(b);
 
-var b;
-console.log(b);
+// Arthematic Operations
+
+console.log(65 + 55);
+console.log(65 - 55);
+console.log(65 * 55);
+console.log(65 / 55);
+console.log(65 % 55);
+
+// String Concatenation
+console.log('Hello' + 'World');
+console.log('Hello' + ' ' + 'World');
+
+// String length
+console.log('Hello'.length); //5
+console.log("My Name is Madhu".length); //16
+
+//String Methods
+console.log('Hello'.toUpperCase());
+console.log('VAnaKam'.toLowerCase());
+console.log('Super Ranga'.startsWith('S'));
+console.log('Super Ranga'.startsWith('R'));
+
+//Build-in Objects
+console.log(Math.random());
+console.log(Math.floor(46.7)); //46
+console.log(Math.ceil(46.7)); //47
+console.log(Number.isInteger(75)); //true
+console.log(Number.isInteger(75.33)); //false
