b04780f7c957054d8d082c404567813418487249
Madhumitha <madhumithavenu03@gmail.com>
2023-07-25 10:23:54 +0530

    JS Datatypes

--- 0A/index.js
+++ 0A/index.js
@@ -1,2 +1,16 @@
 //Javascript is a scripting language which was initially used for Client Side Validation.
-// Now is used for server side programming after the induction of specification ES6. 
+// Now is used for server side programming after the induction of specification ES6.
+
+/*console.log("Hello World");
+console.log("My Name is Madhu");
+console.log(2002);*/
+
+// There are six primary data-types in JavaScript(excluding symbol)
+// a)Number b)String c)Boolean d)Null e)Undefined f)Object
+
+console.log(5);
+console.log(25.69);
+
+console.log('JavaScript');
+console.log('My name is Madhu! I love #JScript');
+
