32f15d5a31c9672ef3a97d8c36511a6f145679b8
Madhumitha <madhumithavenu03@gmail.com>
2023-07-26 09:47:24 +0530

    if,else if Condition

---
+++ 0B/index.js
@@ -0,0 +1,17 @@
+//Conditions and functions
+// In javascript 'if','else if','else' are the conditional Statements
+
+let flag = true;
+if (flag) {
+    console.log("Salute, Salute, Salute");
+}
+
+if (5 > 6){
+    console.log("Do Something");
+}
+else if (7 > 9){
+    console.log("Do Something Something");
+}
+else{
+    console.log("Dont do anything");
+}
