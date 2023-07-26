95c9683c09c2bf62324ba3967facaaecc960c368
Madhumitha <madhumithavenu03@gmail.com>
2023-07-26 10:01:25 +0530

    Non-Boolean inside if

--- 0B/index.js
+++ 0B/index.js
@@ -1,17 +1,24 @@
 //Conditions and functions
 // In javascript 'if','else if','else' are the conditional Statements
 
-let flag = true;
-if (flag) {
-    console.log("Salute, Salute, Salute");
-}
+// let flag = true;
+// if (flag) {
+//     console.log("Salute, Salute, Salute");
+// }
 
-if (5 > 6){
-    console.log("Do Something");
-}
-else if (7 > 9){
-    console.log("Do Something Something");
+
+
+//Non-Boolean inside IF condition
+/*  if we are using non-boolean values as a condition in IF:
+    a) For Number: 0 is considered as false.
+    b) Any other number is considered as false.
+    c) For String '',"",null are considered as false.
+    d) Any undefined or null or NaN(not a number) will be considered as false.*/
+
+let flag = 55.5;
+if(flag){
+    console.log("Flag is Truthy");
 }
 else{
-    console.log("Dont do anything");
+    console.log("Flag is Falsy");
 }
