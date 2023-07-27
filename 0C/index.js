154d8c5bf48b6de56d2b57b690d7d316acfde152
Madhumitha <madhumithavenu03@gmail.com>
2023-07-27 09:59:25 +0530

    Declaration and Initialization of Array

---
+++ 0C/index.js
@@ -0,0 +1,10 @@
+//Arrays & Loops
+/* Arrays are data structures in Javascript which will help store values of same data-type in other languages, but multiple objects in Javascript */
+// let southasia =['India','Pakistan','Bangladesh'];
+// console.log(southasia);
+
+let southasia=[];
+southasia [0] = 'India';
+southasia [1] = 'Pakistan';
+southasia [2] = 'Bangladesh';
+console.log(southasia);
