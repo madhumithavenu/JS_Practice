1632d745c970e03e7d31f1ba19279b22cca9f1fb
Madhumitha <madhumithavenu03@gmail.com>
2023-07-27 10:10:05 +0530

    Accessing Arrays

--- 0C/index.js
+++ 0C/index.js
@@ -8,3 +8,13 @@ southasia [0] = 'India';
 southasia [1] = 'Pakistan';
 southasia [2] = 'Bangladesh';
 console.log(southasia);
+
+//Accessing Arrays
+let southeastAsia = ['India','Pakistan','Bangladesh','Buthan','Burma','Nepal','Loas','Combodia','Vietnam'];
+southeastAsia[4]='Mayanmar';
+console.log("Burma's name has been changed to"+ southeastAsia[4]+ "recently");
+
+let topEconomy = southeastAsia[0];
+console.log(`Richest Economy in South Asia is  ${topEconomy}!!`);
+
+console.log(`If we try to access an array out of index we get :${southeastAsia} `);
