30eeaa8862f9c7f182f7306423ff3e8f87e0ced0
Madhumitha <madhumithavenu03@gmail.com>
2023-07-28 10:30:13 +0530

    Call Back Functions

--- 0D/index.js
+++ 0D/index.js
@@ -10,7 +10,26 @@ addTwoNumbers(5 ,6);// Invoking Method using Variable 1
 addMethod(5, 6); //Invoking Method  using Variable 2
 
 //Prnting Function Name
-console.log(addTwoNumbers);
-console.log(addMethod);
-console.log(addTwoNumbers.name);
-console.log(addMethod.name);
+console.log(addTwoNumbers);//[Function: addTwoNumbers]
+console.log(addMethod);//[Function: addTwoNumbers]
+console.log(addTwoNumbers.name);//addTwoNumbers
+console.log(addMethod.name);//addTwoNumbers
+
+//call Back Functions (Aam Zindagi)
+const add = (x,y)=>{
+    console.log(`Sum of the two numbers: ${x + y}`);
+}
+add(5, 6);
+// const add = function(x,y){
+//     console.log(`Sum of the two numbers: ${x + y}`);
+// }
+
+//(mentos Zindagi)
+const sum = (param)=>{
+    param(5,6);
+    console.log(`Finished adding two numbers`);
+}
+const add1 = (x,y)=>{
+    console.log(`Sum of the two numbers: ${x + y}`);
+}
+sum(add1);
