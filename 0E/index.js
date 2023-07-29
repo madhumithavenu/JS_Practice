9a28db68bb35d4f4e7b3beac5575a87c6ac2a6dd
Madhumitha <madhumithavenu03@gmail.com>
2023-07-29 14:43:23 +0530

    Objects in JS

--- 0E/index.js
+++ 0E/index.js
@@ -14,3 +14,18 @@ console.log(`The dog is weighing ${dog.weight} Kilos`);//The dog is weighing 18 
 let response =dog ['responds to'];
 console.log(`My dog responds to ${response}`);//My dog responds to whistle,clap,throw ball
 console.log(dog.tag_no);//776
+
+let baby ={
+    cat : 'kitten',
+    dog : 'puppy',
+    kangaroo: 'zoey',
+    'horse baby': 'fawn',
+    'baby duck': 'duckling',
+    'lion': 'cub',
+    sheep: 'lamb'
+}
+console.log(baby); //Prints the whole objects
+console.log(baby.cat);//kitten
+console.log(baby['horse baby']);//fawn
+console.log(baby['baby duck']);//duckling
+console.log(baby.lion);//cub
