8e28c8985b4f57cd52da3ac3661b6ab25e650718
Madhumitha <madhumithavenu03@gmail.com>
2023-08-01 10:11:18 +0530

    Another Approach for call Function using Mentos Zindagi

--- 0F/index.js
+++ 0F/index.js
@@ -34,8 +34,25 @@ personTwo.printName();//SachinTendulkar */
 // personOne.printName.call(personTwo);//SachinTendulkar
 
 // Approach 2(Aam Zindagi)
-let printName = function (obj) { 
-    console.log(obj.firstname+ " "+obj.lastname);
+// let printName = function (obj) { 
+//     console.log(obj.firstname+ " "+obj.lastname);
+// }
+
+// let personOne = {
+//     firstname: 'Madhu',
+//     lastname: 'Mitha',
+// }
+
+// let personTwo = {
+//     firstname: "Sachin",
+//     lastname: "Tendulkar",
+// }
+// printName(personOne);
+// printName(personTwo);
+
+//Approach 2(Mentos Zindagi)
+let printName = function () { 
+    console.log(this.firstname+ " "+this.lastname);
 }
 
 let personOne = {
@@ -47,5 +64,5 @@ let personTwo = {
     firstname: "Sachin",
     lastname: "Tendulkar",
 }
-printName(personOne);
-printName(personTwo);
+printName.call(personOne);
+printName.call(personTwo);
