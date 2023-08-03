2a48fc7467fe6f38fc05771a153d9733ce10b3da
Madhumitha <madhumithavenu03@gmail.com>
2023-08-03 09:46:59 +0530

    Working on Constructor

---
+++ 0H/index.js
@@ -0,0 +1,22 @@
+class Box{
+    constructor(l,b,h){
+        this.l = l;
+        this.b = b;
+        this.h = h;
+    }
+    printVolume(){
+        console.log(`Volume of the box is:${this.l * this.b*this.h}`);
+    }
+    fetchSurfaceArea(){
+        return 2 *(this.l* this.b + this.b*this.h+ this.h* this.l);
+    }
+    setBoxSize(l,b,h){
+        this.l = l;
+        this.b = b;
+        this.h = h; 
+    }
+}
+let b =new Box(5,5,5);
+b.printVolume();
+b.setBoxSize(10,11,12);
+b.printVolume();
