6d1af82343c8260b00a0cf828fa22ca0bffa118b
Madhumitha <madhumithavenu03@gmail.com>
2023-08-04 11:12:32 +0530

    Destructing

--- 0J/water-limits.js
+++ 0J/water-limits.js
@@ -2,10 +2,10 @@
 //     return celsius * (9 / 5) + 32;
 // }
 
-const converters = require('./converters');
+const {celsiusToFahrenheit} = require('./converters');
 
-const freezingPointF = converters.celsiusToFahrenheit(0);
-const boilingPointF = converters.celsiusToFahrenheit(100);
+const freezingPointF =celsiusToFahrenheit(0);
+const boilingPointF = celsiusToFahrenheit(100);
 
 console.log(`The freezing point of water in Fahrenheit is ${freezingPointF}`);
 console.log(`The boiling point of water in Fahrenheit is ${boilingPointF}`);
