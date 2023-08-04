79f5470ef2f4cb6a413943d8d49f96c5548ea2d5
Madhumitha <madhumithavenu03@gmail.com>
2023-08-04 12:15:31 +0530

    Another method

--- 0J/converters-ES6.js
+++ 0J/converters-ES6.js
@@ -3,7 +3,7 @@ The node runtime environment and the module.exports and require() syntax.
 The browser's runtime environment and the ES6 we use import/export syntax. */
 
 
-function celsiusToFahrenheit(celsius) {
+/*function celsiusToFahrenheit(celsius) {
     return celsius * (9 / 5) + 32;
 }
 
@@ -11,4 +11,12 @@ function fahrenhietToCelsius(fahrenheit) {
     return (fahrenheit - 32) * (5 / 9);
 }
 
-export { celsiusToFahrenheit, fahrenhietToCelsius };
+export { celsiusToFahrenheit, fahrenhietToCelsius }; */
+
+export function celsiusToFahrenheit(celsius) {
+    return celsius * (9 / 5) + 32;
+}
+
+export function fahrenhietToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * (5 / 9);
+}
