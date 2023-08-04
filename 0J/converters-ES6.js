70515874208bf8fd55e684fdce4b951f641139e8
Madhumitha <madhumithavenu03@gmail.com>
2023-08-04 11:19:48 +0530

    Node Environment

---
+++ 0J/converters-ES6.js
@@ -0,0 +1,14 @@
+/* Node Environment-vs-Browser Environment
+The node runtime environment and the module.exports and require() syntax.
+The browser's runtime environment and the ES6 we use import/export syntax. */
+
+
+function celsiusToFahrenheit(celsius){
+    return celsius * (9 / 5) + 32;
+}
+
+function fahrenhietToCelsius(fahrenheit){
+    return (fahrenheit -32)* (5 /9);
+}
+
+export{ celsiusToFahrenheit , fahrenhietToCelsius};
