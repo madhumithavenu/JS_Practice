20b5b7a3174bd6574ba09e51731a956b2d38c3cc
Madhumitha <madhumithavenu03@gmail.com>
2023-08-04 11:54:55 +0530

    Measuring Human Body Temperature

--- 0J/water-limits-es6.js
+++ 0J/water-limits-es6.js
@@ -7,7 +7,7 @@ function clickMe(){
     let element = document.getElementById("container");
     element.innerHTML= `<h1>Boiling Point is : ${celsiusToFahrenheit(100)}
     & Freezing Point is : ${celsiusToFahrenheit(0)}</h1>
-    <h2>Human Body Temperature is : ${fahrenhietToCelsius(98.6)} celsius`;
+    <h2>Human Body Temperature is: ${fahrenhietToCelsius(98.6)} celsius`;
 }
 
 console.log(`Water Boiling point is ${celsiusToFahrenheit(100)} Degree Fahrenhiet`);
