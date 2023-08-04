0436eeda9c143dbf29ebe13269fea189d1dbb026
Madhumitha <madhumithavenu03@gmail.com>
2023-08-04 11:54:13 +0530

    Measuring Water boiling point

--- 0J/water-limits-es6.js
+++ 0J/water-limits-es6.js
@@ -1,12 +1,13 @@
-import { celsiusToFahrenheit, fahrenhietToCelsius } from "./converters-ES6";
+import { celsiusToFahrenheit, fahrenhietToCelsius } from "./converters-es6.js";
 
 const buttonElement = document.getElementById('button');
 buttonElement.addEventListener('click',clickMe);
 
 function clickMe(){
     let element = document.getElementById("container");
-    element.innerHTML= <h1>Boiling Point is : ${celsiusToFahrenheit(100)}
+    element.innerHTML= `<h1>Boiling Point is : ${celsiusToFahrenheit(100)}
     & Freezing Point is : ${celsiusToFahrenheit(0)}</h1>
+    <h2>Human Body Temperature is : ${fahrenhietToCelsius(98.6)} celsius`;
 }
 
 console.log(`Water Boiling point is ${celsiusToFahrenheit(100)} Degree Fahrenhiet`);
