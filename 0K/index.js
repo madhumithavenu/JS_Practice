efc10f1e5f97b5e6d8900ba9bc9af3ddec26fd1e
Madhumitha <madhumithavenu03@gmail.com>
2023-08-07 15:55:48 +0530

    Handling Promises in JS

--- 0K/index.js
+++ 0K/index.js
@@ -52,25 +52,25 @@ In Asynchronous programming language like a JS: Callback function are absolutely
 */
 
 const profiles = {
-    'balaji':{
+    'balaji': {
         fullname: 'Balaji KR',
         age: 35,
         married: true,
-        hobbies: ['Defence','Finance','Foreign Affairs']
+        hobbies: ['Defence', 'Finance', 'Foreign Affairs']
     },
-    'seema':{
+    'seema': {
         fullname: 'Seema H',
         age: 30,
         married: true,
-        hobbies: ['Cooking','Painting','Art & Crafts']
+        hobbies: ['Cooking', 'Painting', 'Art & Crafts']
     },
-    'kishen':{
+    'kishen': {
         fullname: 'Kishen KB',
         age: 3,
         married: false,
-        hobbies: ['football','Video games']
+        hobbies: ['football', 'Video games']
     },
-    'rajeev':{
+    'rajeev': {
         fullname: 'Rajeevalochana KV',
         age: 70,
         married: true,
@@ -96,44 +96,90 @@ const profiles = {
  goToHell(profiles); */
 
 //Stage-2
-function goToHell(pros){
-    setTimeout((objs)=>{
+function goToHell(pros) {
+    setTimeout((objs) => {
         let keys = Object.keys(objs);
         console.log(keys);
-        setTimeout((key,objs)=>{
+        setTimeout((key, objs) => {
             console.log(objs[key]);
 
-            setTimeout((sub)=>{
+            setTimeout((sub) => {
                 console.log(`Age of Kishen is: ${sub.age}`);
-                setTimeout((sub)=>{
+                setTimeout((sub) => {
                     console.log(`Hobbies of Kishen are: ${sub.hobbies}`);
-                },4000,sub);
-            },2000,objs['kishen']);
-        },3000, 'kishen',objs);
-    },2000,pros);
+                }, 4000, sub);
+            }, 2000, objs['kishen']);
+        }, 3000, 'kishen', objs);
+    }, 2000, pros);
 }
- goToHell(profiles);
+goToHell(profiles);
 
- /* Promises in JS
- Promises in JS are used to handle Asynchronous Operations.
- A promise is an object that keep track about whether a certain event has happened already or not.
- It also determines what will happen after the event.
- Promises has three stages: Pending, Completed, Rejected. */
+/* Promises in JS
+Promises in JS are used to handle Asynchronous Operations.
+A promise is an object that keep track about whether a certain event has happened already or not.
+It also determines what will happen after the event.
+Promises has three stages: Pending, Completed, Rejected. */
 
- const bringChocolates = function(){
+/*const bringChocolates = function(){
+   let didIBringTheChocolates = false;
+   // console.log("Getting ready to go to market");
+   // console.log("Booking a cab to the store");
+   // console.log("Selecting the chocolate");
+   // console.log("Watching the payment");
+
+   didIBringTheChocolates = false;
+
+   if(didIBringTheChocolates==true){
+       console.log(`Kishen says: Thank you appa!!`);
+   }
+   else{
+       console.log(`Kishen is crying: I hate you!!`);
+   }
+}
+bringChocolates();
+ 
+Handling Promises
+Promises have a structured way to handle different condition.
+Plain if/else condition does not give us the ability to handle yhings in a structured way.
+If promises carefully executed: resolve() method is executed.
+If promises have failed in executing : reject() method will be executed.
+In the meantime the browser will be continuing other work pending in its hands.
+
+To create a promise we write:
+let anyPromise = new Promise(resolve, reject);
+
+To execute a promise we write:
+anyPromise.then(resolve-fun).catch(reject-fun);
+ 
+*/
+
+const bringChocolates = function (resolve, reject) {
     let didIBringTheChocolates = false;
-    // console.log("Getting ready to go to market");
-    // console.log("Booking a cab to the store");
-    // console.log("Selecting the chocolate");
-    // console.log("Watching the payment");
+    setTimeout(() => { console.log("Getting ready to go to market") }, 2000);
+    setTimeout(() => { console.log("Booking a cab to the store") }, 4000);
+    setTimeout(() => { console.log("Selecting the chocolate") }, 6000);
+    setTimeout(() => { console.log("Making the payment") }, 8000);
 
-    didIBringTheChocolates = false;
+    setTimeout(() => {
+        didIBringTheChocolates = true;
 
-    if(didIBringTheChocolates==true){
-        console.log(`Kishen says: Thank you appa!!`);
-    }
-    else{
-        console.log(`Kishen is crying: I hate you!!`);
-    }
- }
- bringChocolates();
+        if (didIBringTheChocolates == true) {
+            resolve();
+        }
+        else {
+            reject();
+        }
+
+    },12000);
+}
+
+function eatTheChocolates(){
+    console.log(`Kishen says: Thank you appa!!`);
+}
+
+function cryInCorner(){
+    console.log(`Kishen is crying: I hate you`);
+}
+
+let iPromiseToBringChocolates = new Promise(bringChocolates);
+iPromiseToBringChocolates.then(eatTheChocolates).catch(cryInCorner);
