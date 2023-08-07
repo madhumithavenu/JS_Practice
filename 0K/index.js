25cbdd097a1795744aa971c2c434b8433e891e4d
Madhumitha <madhumithavenu03@gmail.com>
2023-08-07 10:11:20 +0530

    Call Back function

---
+++ 0K/index.js
@@ -0,0 +1,14 @@
+let balajiBath = function(){
+    console.log(`\n Balaji Woke Up`);
+    console.log(`Balaji is Bathing`);
+    console.log(`Balaji is Ready`);
+}
+let kishenBath = function(){
+    console.log(`\n Kishen Woke up`);
+    console.log(` Kishen is Bathing`);
+    console.log(` Kishen is Ready`);
+}
+// balajiBath();
+// kishenBath();
+kishenBath();
+balajiBath();
