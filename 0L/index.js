9916a07db94054e515dc555f455d3305dcb4c1f9
Madhumitha <madhumithavenu03@gmail.com>
2023-08-08 10:49:08 +0530

    Async functions

---
+++ 0L/index.js
@@ -0,0 +1,14 @@
+/*function balaji(){
+    return 'balaji';
+}
+
+let b = balaji();
+console.log(b);  Returning a text.*/
+
+//Async Functions Return promise
+async function balaji(){
+    return 'balaji';
+}
+
+let b = balaji();
+console.log(b);
